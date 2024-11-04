b7a9dfdc39019a9dfc61787d3684da7f84aa1416
rajacloud2244 <spandanapaladi@outlook.com>
2024-11-04 21:27:16 +0530

    Update variables.tf

--- alb/variables.tf
+++ alb/variables.tf
@@ -1,100 +1,47 @@
 variable "create" {
-  type    = bool
-  default = true
+  description = "Whether to create the ALB"
+  type        = bool
+  default     = true
 }
 
-
 variable "access_logs" {
-  type = list(object({
-    bucket  = string
+  description = "Access logs configuration"
+  type        = list(object({
     enabled = bool
-    prefix  = optional(string)
+    bucket  = string
+    prefix  = string
   }))
-  default = []
+  default     = []
 }
 
 variable "subnets" {
-  type = list(string)
+  description = "Subnets for the ALB"
+  type        = list(string)
 }
-
-variable "tags" {
-  type = map(string)
-}
-
-
 
 variable "load_balancer_name" {
-  type = string
+  description = "Name of the load balancer"
+  type        = string
 }
 
 variable "target_group_count" {
-  type    = number
-  default = 1
+  description = "Number of target groups"
+  type        = number
 }
 
 variable "target_group_port" {
-  type    = number
-  default = 80
+  description = "Port for the target group"
+  type        = number
 }
 
 variable "target_group_protocol" {
-  type    = string
-  default = "HTTP"
+  description = "Protocol for the target group"
+  type        = string
 }
 
 variable "vpc_id" {
-  type = string
-}
-
-variable "health_check_path" {
-  type    = string
-  default = "/"
-}
-
-variable "health_check_interval" {
-  type    = number
-  default = 30
-}
-
-variable "health_check_timeout" {
-  type    = number
-  default = 5
-}
-
-variable "health_check_healthy_threshold" {
-  type    = number
-  default = 2
-}
-
-variable "health_check_unhealthy_threshold" {
-  type    = number
-  default = 2
-}
-
-
-
-variable "ec2_instance_ids" {
-  type = list(string)
-}
-
-
-variable "load_balancer_arn" {
-  type = string
-}
-
-variable "listeners" {
-  type = map(object({
-    port            = optional(number, 80)
-    protocol        = optional(string, "HTTP")
-    default_actions = list(object({
-      type            = string
-      target_groups   = list(object({
-        target_group_key = string
-        weight           = optional(number)
-      }))
-      order           = optional(number, 1)
-    }))
-  }))
+  description = "VPC ID where the ALB is deployed"
+  type        = string
 }
 
 variable "target_group_arn" {
@@ -103,3 +50,4 @@ variable "target_group_arn" {
   default     = null
 }
 
+# Other variables as needed...
