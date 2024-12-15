eac09ab78f6c4b9fcee5f932bc7bfe95c18c098a
rajacloud2244 <spandanapaladi@outlook.com>
2024-12-15 20:09:24 +0530

    Update efs.tf

--- efs/efs.tf
+++ efs/efs.tf
@@ -1,1 +1,31 @@
+################################################################################
+# File System
+################################################################################
+
+resource "aws_efs_file_system" "this" {
+  count = var.create ? 1 : 0
+
+  availability_zone_name          = var.availability_zone_name
+  creation_token                  = var.creation_token
+  performance_mode                = var.performance_mode
+  encrypted                       = var.encrypted
+  #kms_key_id                      = var.kms_key_arn
+  provisioned_throughput_in_mibps = var.provisioned_throughput_in_mibps
+  throughput_mode                 = var.throughput_mode
+
+  dynamic "lifecycle_policy" {
+    for_each = [for k, v in var.lifecycle_policy : { (k) = v }]
+
+    content {
+      transition_to_ia                    = try(lifecycle_policy.value.transition_to_ia, null)
+      transition_to_archive               = try(lifecycle_policy.value.transition_to_archive, null)
+      transition_to_primary_storage_class = try(lifecycle_policy.value.transition_to_primary_storage_class, null)
+    }
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = var.name },
+  )
+}
 
