8167890ce430cb755e0c617ba01e278ad3d8fca0
rajacloud2244 <spandanapaladi@outlook.com>
2024-12-15 20:09:53 +0530

    Update iam.tf

--- efs/iam.tf
+++ efs/iam.tf
@@ -1,1 +1,108 @@
+################################################################################
+# File System Policy
+################################################################################
+
+data "aws_iam_policy_document" "policy" {
+  count = var.create && var.attach_policy ? 1 : 0
+
+  source_policy_documents   = var.source_policy_documents
+  override_policy_documents = var.override_policy_documents
+
+  dynamic "statement" {
+    for_each = var.policy_statements
+
+    content {
+      sid           = try(statement.value.sid, null)
+      actions       = try(statement.value.actions, null)
+      not_actions   = try(statement.value.not_actions, null)
+      effect        = try(statement.value.effect, null)
+      resources     = try(statement.value.resources, [aws_efs_file_system.this[0].arn], null)
+      not_resources = try(statement.value.not_resources, null)
+
+      dynamic "principals" {
+        for_each = try(statement.value.principals, [])
+
+        content {
+          type        = principals.value.type
+          identifiers = principals.value.identifiers
+        }
+      }
+
+      dynamic "not_principals" {
+        for_each = try(statement.value.not_principals, [])
+
+        content {
+          type        = not_principals.value.type
+          identifiers = not_principals.value.identifiers
+        }
+      }
+
+      dynamic "condition" {
+        for_each = try(statement.value.conditions, statement.value.condition, [])
+
+        content {
+          test     = condition.value.test
+          values   = condition.value.values
+          variable = condition.value.variable
+        }
+      }
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.deny_nonsecure_transport ? [1] : []
+
+    content {
+      sid       = "NonSecureTransport"
+      effect    = "Deny"
+      actions   = ["*"]
+      resources = [aws_efs_file_system.this[0].arn]
+
+      principals {
+        type        = "AWS"
+        identifiers = ["*"]
+      }
+
+      condition {
+        test     = "Bool"
+        variable = "aws:SecureTransport"
+        values   = ["false"]
+      }
+    }
+  }
+
+  dynamic "statement" {
+    for_each = var.deny_nonsecure_transport_via_mount_target ? [1] : []
+
+    content {
+      sid    = "NonSecureTransportAccessedViaMountTarget"
+      effect = "Allow"
+      actions = [
+        "elasticfilesystem:ClientRootAccess",
+        "elasticfilesystem:ClientWrite",
+        "elasticfilesystem:ClientMount"
+      ]
+      resources = [aws_efs_file_system.this[0].arn]
+
+      principals {
+        type        = "AWS"
+        identifiers = ["*"]
+      }
+
+      condition {
+        test     = "Bool"
+        variable = "elasticfilesystem:AccessedViaMountTarget"
+        values   = ["true"]
+      }
+    }
+  }
+}
+
+resource "aws_efs_file_system_policy" "this" {
+  count = var.create && var.attach_policy ? 1 : 0
+
+  file_system_id                     = aws_efs_file_system.this[0].id
+  bypass_policy_lockout_safety_check = var.bypass_policy_lockout_safety_check
+  policy                             = data.aws_iam_policy_document.policy[0].json
+}
 
